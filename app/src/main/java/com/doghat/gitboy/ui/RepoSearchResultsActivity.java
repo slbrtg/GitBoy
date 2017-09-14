cc630a689cfdc7fb805153149be011581ff71e8e
Saul Artiga <slbrtg@gmail.com>
2017-09-14 14:05:01 -0700

    add arrayadapter to display static data

--- app/src/main/java/com/doghat/gitboy/ui/RepoSearchResultsActivity.java
+++ app/src/main/java/com/doghat/gitboy/ui/RepoSearchResultsActivity.java
@@ -3,6 +3,7 @@ package com.doghat.gitboy.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,9 +25,13 @@ public class RepoSearchResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_repo_search_results);
         ButterKnife.bind(this);
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, repos);
+        mRepoListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String searchRepoQuery = intent.getStringExtra("searchRepoQuery");
         mSearchRepoResultsTextView.setText("You searched for: " + searchRepoQuery);
+
 
     }
 }
