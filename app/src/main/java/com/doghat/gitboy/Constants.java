8b681fadd9b43fc8dab10a040ed950fe21cf8b32
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-14 16:01:17 -0700

    begin adding constants for api integration

---
+++ app/src/main/java/com/doghat/gitboy/Constants.java
@@ -0,0 +1,7 @@
+package com.doghat.gitboy;
+
+
+public class Constants {
+    public static final String GITHUB_CLIENT_ID = BuildConfig.GITHUB_CLIENT_ID;
+    public static final String GITHUB_CLIENT_SECRET = BuildConfig.GITHUB_CLIENT_SECRET;
+}
