5584a39b5582bfe2ae1ba74194923823fd38b1c6
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-14 16:42:28 -0700

    work on layout

--- app/src/main/java/com/doghat/gitboy/Constants.java
+++ app/src/main/java/com/doghat/gitboy/Constants.java
@@ -4,4 +4,9 @@ package com.doghat.gitboy;
 public class Constants {
     public static final String GITHUB_CLIENT_ID = BuildConfig.GITHUB_CLIENT_ID;
     public static final String GITHUB_CLIENT_SECRET = BuildConfig.GITHUB_CLIENT_SECRET;
+    public static final String GITHUB_BASE_URL = "https://api.github.com/";
+    public static final String GITHUB_REPO_QUERY_PARAMETER = "repos";
+
 }
+
+
