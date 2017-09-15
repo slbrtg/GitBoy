52632c504bac4afd4155f3b6547d607336fd6dbd
Saul Artiga <slbrtg@gmail.com>
2017-09-15 13:47:51 -0700

    add model for repo

---
+++ app/src/main/java/com/doghat/gitboy/models/Repo.java
@@ -0,0 +1,69 @@
+package com.doghat.gitboy.models;
+
+public class Repo {
+    private String mName;
+    private String mDescription;
+    private String mHtmlUrl;
+    private Boolean mIsPrivate;
+    private String mLanguage;
+    private String mOwner;
+    private String mOwnerType;
+    private String mOwnerAvatarUrl;
+    private String mOwnerProfileHtmlUrl;
+    private String mOwnerReposHtmlUrl;
+
+    public Repo(String name, String description, String htmlUrl, Boolean isPrivate,
+                String language, String owner, String ownerType, String ownerAvatarUrl,
+                String ownerProfileHtmlUrl) {
+        this.mName = name;
+        this.mDescription = description;
+        this.mHtmlUrl = htmlUrl;
+        this.mIsPrivate = isPrivate;
+        this.mLanguage = language;
+        this.mOwner = owner;
+        this.mOwnerType = ownerType;
+        this.mOwnerAvatarUrl = ownerAvatarUrl;
+        this.mOwnerProfileHtmlUrl = ownerProfileHtmlUrl;
+        this.mOwnerReposHtmlUrl = ownerProfileHtmlUrl + "/repos";
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public String getmHtmlUrl() {
+        return mHtmlUrl;
+    }
+
+    public Boolean getmIsPrivate() {
+        return mIsPrivate;
+    }
+
+    public String getmLanguage() {
+        return mLanguage;
+    }
+
+    public String getmOwner() {
+        return mOwner;
+    }
+
+    public String getmOwnerType() {
+        return mOwnerType;
+    }
+
+    public String getmOwnerAvatarUrl() {
+        return mOwnerAvatarUrl;
+    }
+
+    public String getmOwnerProfileHtmlUrl() {
+        return mOwnerProfileHtmlUrl;
+    }
+
+    public String getmOwnerReposHtmlUrl() {
+        return mOwnerReposHtmlUrl;
+    }
+}
