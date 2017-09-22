f9098a9493d56f66c25730f1db1ba824de6e46df
Saul Artiga <slbrtg@gmail.com>
2017-09-22 12:47:33 -0700

    add ui for login and create account

---
+++ app/src/main/java/com/doghat/gitboy/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.doghat.gitboy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.doghat.gitboy.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
